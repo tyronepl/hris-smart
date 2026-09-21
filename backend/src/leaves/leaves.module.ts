dd1afc88b58726fd55799da45e1348e2a1b5e3d4
tyrone <tyronelegislador@gmail.com>
2026-09-22 07:51:02 +0800

    fix: authmodule missing from leaves

--- backend/src/leaves/leaves.module.ts
+++ backend/src/leaves/leaves.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AuthModule } from '../auth/auth.module';
+
 import { Leave } from './leave.entity';
 import { LeavesController } from './leaves.controller';
 import { LeavesService } from './leaves.service';
 
 @Module({
   imports: [
+    AuthModule,
     TypeOrmModule.forFeature([
       Leave,
     ]),
